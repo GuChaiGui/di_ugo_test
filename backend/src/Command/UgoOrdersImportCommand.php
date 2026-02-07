33baff464c499daf9ebfa1cef9857ec547b374c7
ly_e <ly_e@etna-alternance.net>
2026-02-07 18:35:06 +0100

    fix CSV importer Service

--- backend/src/Command/UgoOrdersImportCommand.php
+++ backend/src/Command/UgoOrdersImportCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Service\CsvImporter;
 
 #[AsCommand(
     name: 'ugo:orders:import',
@@ -16,9 +17,12 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class UgoOrdersImportCommand extends Command
 {
-    public function __construct()
+    private CsvImporter $importer;
+
+    public function __construct(CsvImporter $importer)
     {
         parent::__construct();
+        $this->importer = $importer;
     }
 
     protected function configure(): void
@@ -32,17 +36,14 @@ class UgoOrdersImportCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $csvDir = __DIR__ . '/../../data/';
+        $customersFile = $csvDir . 'customers.csv';
+        $ordersFile = $csvDir . 'purchases.csv';
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+        $this->importer->import($customersFile, $ordersFile);
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success('CSV import completed successfully!');
 
         return Command::SUCCESS;
     }
